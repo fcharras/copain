8a8918918510efdd493f1cae4126d186fa05906c
franck <franck@aldana.localdomain>
2021-12-20 22:24:56 +0100

    brute force kind of works... except for last boss. step before aiming to implement a macro / micro approach for space searching

--- copain/commands/gradius_brute_force.py
+++ copain/commands/gradius_brute_force.py
@@ -17,11 +17,10 @@ NUM_RUNNERS = 1
 THREADED_SOCKET = True
 THREADED_REQUESTS = True
 
-FRAME_PER_ACTION = 5
+FRAME_PER_ACTION = 16
 NUMBER_OF_SAVESTATES = 10
 ACTIONS_PER_SAVESTATES = 12
 
-import random
 from collections import deque
 from weakref import WeakKeyDictionary
 
@@ -35,6 +34,32 @@ class GradiusLoopFn:
     ONE = np.uint16(1)
     bONE = ONE.tobytes()
     IS_ALIVE_ADDRESS = np.uint16(0x100).tobytes()
+
+    X_AXIS_ADDRESS = np.uint16(0x360).tobytes()  # range 16 (left) - 240 (right)
+    X_AXIS_BALANCE = np.uint16(80)
+    # X_AXIS_BALANCE = np.uint16(146)
+    Y_AXIS_ADDRESS = np.uint16(0x320).tobytes()  # range 16 (top) - 192 (right)
+    # Y_AXIS_BALANCE = np.uint16(96)
+    Y_AXIS_BALANCE = np.uint16(50)
+
+    DIRECTION_VECTORS = np.array(
+        [
+            [0, 0],
+            [0, -1],
+            [1, -1],
+            [1, 0],
+            [1, 1],
+            [0, 1],
+            [-1, 1],
+            [-1, 0],
+            [-1, -1],
+        ],
+        dtype=np.float16,
+    )
+
+    DIRECTION_VECTORS[1:] /= np.linalg.norm(
+        DIRECTION_VECTORS[1:], ord=2, axis=1, keepdims=True
+    )
 
     def __init__(self, frame_per_action, number_of_savestates, actions_per_savestate):
         self.frame_per_action = frame_per_action
@@ -51,7 +76,6 @@ class GradiusLoopFn:
             self.handler.savestate_object() for i in range(self.number_of_savestates)
         )
         savestates_state_id = WeakKeyDictionary()
-
         dead_ends = set()
 
         state_id = None
@@ -83,6 +107,7 @@ class GradiusLoopFn:
 
                 self.handler.savestate_load(savestate)
                 state_id = savestate_id
+                actions_since_savestate = 0
                 continue
 
             inputs = self._get_direction_as_joypad_inputs(direction)
@@ -102,6 +127,7 @@ class GradiusLoopFn:
                 self.handler.savestate_load(savestate)
                 state_id = savestate_id
                 reset = False
+                actions_since_savestate = 0
                 continue
 
             if actions_since_savestate < self.actions_per_savestates:
@@ -117,6 +143,8 @@ class GradiusLoopFn:
             savestates_state_id[savestate] = savestate_id = state_id
             self.handler.savestate_save(savestate)
             self.handler.savestate_persist(savestate)
+
+            actions_since_savestate = 0
 
     def _register_handler(self, handler):
         self.handler = handler
@@ -144,25 +172,39 @@ class GradiusLoopFn:
             == 1
         )
 
+    def _xy_coordinates(self):
+        return (
+            self.handler.memory_readbyterange(
+                self.X_AXIS_ADDRESS,
+                self.ONE,
+                self.bONE,
+            )[0],
+            self.handler.memory_readbyterange(
+                self.Y_AXIS_ADDRESS,
+                self.ONE,
+                self.bONE,
+            )[0],
+        )
+
     def _get_next_direction(self, state_id, dead_ends):
-        possible_directions_ids = [
-            hash((state_id, direction)) for direction in range(9)
-        ]
+        x, y = self._xy_coordinates()
 
-        default_direction_id = possible_directions_ids[0]
-        if not default_direction_id in dead_ends:
-            return 0, default_direction_id
+        direction_toward_balance = np.array(
+            (self.X_AXIS_BALANCE - x, self.Y_AXIS_BALANCE - y), dtype=np.int16
+        )
 
-        possible_directions = [
-            (direction, direction_id)
-            for direction, direction_id in zip(range(1, 9), possible_directions_ids[1:])
-            if not direction_id in dead_ends
-        ]
+        preferred_directions = (
+            -np.matmul(self.DIRECTION_VECTORS, direction_toward_balance)
+        ).argsort(kind="stable")
 
-        if not possible_directions:
-            raise DeadEnd(possible_directions_ids)
+        direction_ids = []
+        for direction in preferred_directions:
+            direction_id = hash((state_id, int(direction)))
+            direction_ids.append(direction_id)
+            if direction_id not in dead_ends:
+                return int(direction), direction_id
 
-        return random.Random(state_id).choice(possible_directions)
+        raise DeadEnd(direction_ids)
 
     def _get_direction_as_joypad_inputs(self, direction):
         if direction == 0:
